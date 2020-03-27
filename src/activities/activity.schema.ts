b05a6bc69695e46ff25f45cdc3a5e7ef496fcbde
Benjamin Kim <benkim96@gmail.com>
2020-03-26 23:32:57 -0700

    change activity schema ReplySchema referencing

--- src/activities/activity.schema.ts
+++ src/activities/activity.schema.ts
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 
 const ReplySchema = Schema({
     _id: { type: Schema.Types.ObjectId, auto: true },
-    idUser: { type: Schema.Types.ObjectId, ref: 'Users' },
+    // idUser: { type: Schema.Types.ObjectId, ref: 'Users' },
+    idUser: { type: String },
     publishDate: { type: Date, default: Date.now() },
     content: { type: String, required: true},
     score: { type: Number, required: true, default: 0 },
@@ -17,7 +18,8 @@ const CommentSchema = Schema({
     publishDate: { type: Date, default: Date.now() },
     content: { type: String, required: true},
     score: { type: Number, required: true, default: 0 },
-    reply: [{ type: Schema.Types.ObjectId, ref: 'ReplySchema' }],
+	// reply: [{ type: Schema.Types.ObjectId, ref: 'ReplySchema' }],
+	reply: [ReplySchema]
 });
 
 const ReactionSchema = Schema({
