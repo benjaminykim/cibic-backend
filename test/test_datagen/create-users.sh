c7a06f61d55f52143177a1f25adc9a824a6d0df7
Benjamin Kim <benkim96@gmail.com>
2020-03-27 21:31:38 -0700

    [UPDATE] test data population script user, cabildo, following

---
+++ test/test_datagen/create-users.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+usernames=(     "tmonroe"
+                "bekim"
+                "mschmidt"
+                "epuculla"
+                "janog"
+                "dloza"
+                "emiliano"
+                "martiix"
+                "maticlarke"
+)
+
+firstnames=(    "Steven"
+                "Ben"
+                "Mike"
+                "Edith"
+                "Jano"
+                "Daiana"
+                "Emiliano"
+                "Martin"
+                "Matias"
+)
+
+lastnames=( "lname"
+            "lname"
+            "lname"
+            "lname"
+            "lname"
+            "lname"
+            "lname"
+            "lname"
+            "lname"
+            )
+
+temp=curlResponse.log
+output=idUser.txt
+
+rm $output
+
+for i in ${!usernames[@]}; do
+    curl -H "Content-Type: application/json" -d'{"user":{
+    "username": "'${usernames[$i]}'",
+    "email": "'${usernames[$i]}'@gmail.fake",
+    "password": "arealpassword",
+    "firstName": "'${firstnames[$i]}'",
+    "middleName": "middleName",
+    "lastName": "'${lastnames[$i]}'",
+    "maidenName": "maidenName",
+    "phone": 123456789,
+    "rut": "1234567891",
+    "cabildos": [],
+    "files": "none",
+    "followers": [],
+    "following": [],
+    "activityFeed": []
+    }}' http://localhost:3000/users >> $temp 2>&1
+done
+grep -o ':".*"' curlResponse.log | sed 's/[:"]//g' >> $output
+rm $temp
+cat $output
