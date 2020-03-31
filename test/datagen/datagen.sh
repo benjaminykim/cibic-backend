9d8b0b0c54e8f1b9a8bfa1c243753e4114eabb70
Benjamin Kim <benkim96@gmail.com>
2020-03-30 18:15:15 -0700

    create activities script

--- test/datagen/datagen.sh
+++ test/datagen/datagen.sh
@@ -6,7 +6,7 @@ sh ./create-users.sh
 sh ./create-cabildos.sh
 sh ./follow-users.sh
 sh ./follow-cabildos.sh
-#sh ./create-activities.sh
+sh ./create-activities.sh
 
 #rm -f ~/idUser.txt
 #rm -f ~/idCabildo.txt
