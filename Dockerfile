79d747df0a2779021a4d62307fb45c3eb15c6b2b
smonroe4242 <scmonroe96@gmail.com>
2020-04-16 14:49:57 -0700

    https now works. enter scripts directory and run ./dev-env-setup, type the password it tells you to, and you'll be able to access the server through https.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:12.16.1-alpine
 WORKDIR /app
 
 COPY . /app
-RUN apk add git; npm install -g jest;	npm install
+RUN apk add git && npm install
 
 EXPOSE 3000
 CMD [ "npm", "run", "start:dev" ]
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ const fs = require('fs');
 
 async function bootstrap() {
     const httpsOptions = {
-        key: fs.readFileSync('./api.cibic.io.key'),
-        cert: fs.readFileSync('./api.cibic.io.crt'),
+        key: fs.readFileSync('./src/api.cibic.io.key'),
+        cert: fs.readFileSync('./src/api.cibic.io.crt'),
     };
     const app = await NestFactory.create(AppModule, { httpsOptions });
     await app.listen(3000);
